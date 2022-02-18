488a3ca5df7db85958dfae2f0652a12b7443a930
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-02-18 12:23:19 -0500

    Added Project Files for Lab 6

---
+++ Lab6/Assets/[Scripts]/HealthBarController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    public Slider HealthBar;
+
+    
+    public void TakeDamage(int damage)
+    {
+        HealthBar.value -= damage;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(10);
+        }
+    }
+}
