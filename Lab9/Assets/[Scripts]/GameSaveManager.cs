0e53c09227026ce6c8feaf800987c85b89fc35c0
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-03-18 11:44:18 -0400

    Added Saving / Loading with PlayerPrefs

--- Lab9/Assets/[Scripts]/GameSaveManager.cs
+++ Lab9/Assets/[Scripts]/GameSaveManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameSaveManager : MonoBehaviour
 {
+    public Transform playerTransform;
+
     // Update is called once per frame
     void Update()
     {
@@ -29,18 +31,35 @@ public class GameSaveManager : MonoBehaviour
         LoadGame();
     }
 
+    // Serializing Data (Encoding)
     public void SaveGame()
     {
-
+        PlayerPrefs.SetString("PlayerPosition", JsonUtility.ToJson(playerTransform.position)); 
+        PlayerPrefs.SetString("PlayerRotation", JsonUtility.ToJson(playerTransform.rotation.eulerAngles));
+        PlayerPrefs.Save();
+        Debug.Log("Game data saved!");
     }
 
+    // Deserializing Data (Decoding)
     public void LoadGame()
     {
-
+        if (PlayerPrefs.HasKey("PlayerPosition"))
+        {
+            playerTransform.gameObject.GetComponent<CharacterController>().enabled = false;
+            playerTransform.position = JsonUtility.FromJson<Vector3>(PlayerPrefs.GetString("PlayerPosition"));
+            playerTransform.rotation = Quaternion.Euler(JsonUtility.FromJson<Vector3>(PlayerPrefs.GetString("PlayerRotation")));
+            playerTransform.gameObject.GetComponent<CharacterController>().enabled = true;
+            Debug.Log("Game data loaded!");
+        }
+        else
+        {
+            Debug.LogError("There is no save data!");
+        }
     }
 
     public void ResetData()
     {
-
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Data reset complete");
     }
 }
