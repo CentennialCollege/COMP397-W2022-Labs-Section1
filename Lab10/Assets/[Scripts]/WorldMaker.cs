49168258273ae7db4435319233f0aad52689aaf8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-03-25 11:44:04 -0400

    Add Project Files for Lab 10

---
+++ Lab10/Assets/[Scripts]/WorldMaker.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMaker : MonoBehaviour
+{
+    [Header("Player Properties")]
+    public GameObject playerPrefab;
+
+    [Header("World Properties")]
+    [Range(8, 64)]
+    public int height = 8;
+    [Range(8, 64)]
+    public int width = 8;
+    [Range(8, 64)]
+    public int depth = 8;
+
+    [Header("Scaling Values")]
+    [Range(8, 64)]
+    public float min = 16.0f;
+    [Range(8, 64)]
+    public float max = 24.0f;
+
+    [Header("Tile Properties")]
+    public Transform tileParent;
+    public GameObject threeDTile;
+
+    [Header("Grid")]
+    public List<GameObject> grid;
+
+    // starting values
+    private int startHeight;
+    private int startWidth;
+    private int startDepth;
+    private float startMin;
+    private float startMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new List<GameObject>(); // creates a new empty container
+
+        Generate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(height != startHeight || depth != startDepth || width != startWidth || min != startMin || max != startMax)
+        {
+            Generate();
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Generate();
+        }
+    }
+
+    private void Generate()
+    {
+        Initialize();
+        Reset();
+        Regenerate();
+    }
+
+    private void Initialize()
+    {
+        startHeight = height;
+        startWidth = width;
+        startDepth = depth;
+        startMin = min;
+        startMax = max;
+    }
+
+    private void Regenerate()
+    {
+        // world generation happens here
+
+        float randomSample = Random.Range(min, max);
+        float offsetX = Random.Range(-1024.0f, 1024.0f);
+        float offsetZ = Random.Range(-1024.0f, 1024.0f);
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int z = 0; z < depth; z++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var perlinValue = Mathf.PerlinNoise((x + offsetX) / randomSample, (z + offsetZ) / randomSample) * depth * 0.5f;
+
+                    if (y < perlinValue)
+                    {
+                        var tile = Instantiate(threeDTile, new Vector3(x, y, z), Quaternion.identity);
+                        tile.transform.parent = tileParent;
+                        grid.Add(tile);
+                    }
+                }
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        foreach (var tile in grid)
+        {
+            Destroy(tile);
+        }
+        grid.Clear();
+    }
+}
